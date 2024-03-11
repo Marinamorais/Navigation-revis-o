134642e98f7aa9093808057abcbb675723bd4ee5
Marinamorais <marinamorais@aluno.senai.br>
2024-03-11 15:11:23 -0300

    stack navigation

---
+++ App.jsx
@@ -0,0 +1,5 @@
+import Routes from "./src/routes/index.routes";
+
+export default function App() {
+  return <Routes />;
+}
---
+++ src/routes/index.routes.jsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native";
+import StackRoutes from "./stack.routes";
+import { StatusBar } from "expo-status-bar";
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <StackRoutes />
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
